21a9fce47c4d7aec042dece687c5dea6afa1d81d
Legenda <schurik458@gmail.com>
2024-03-17 20:24:44 +0200

    car details improve

--- web/src/page/CarDetailsPage.tsx
+++ web/src/page/CarDetailsPage.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
-import {Badge, Box, Breadcrumbs, Container, Link, Paper, Stack, Typography} from "@mui/material";
+import {Badge, Box, Breadcrumbs, Container, Divider, Link, Paper, Stack, Typography} from "@mui/material";
 import Carousel from "react-material-ui-carousel";
 import {CarType} from "../type/car-type";
 import CarService from "../service/car-service";
@@ -59,46 +59,48 @@ function CarDetailsPage() {
         </Carousel>
       </Box>
 
-      <Paper elevation={3}>
+      <Paper elevation={3} sx={{textAlign: {xs: 'center', md: 'left'}}}>
         <Grid2 p={1} container sx={{background: grey[800], color: '#fff'}}>
-          <Grid2 xs={6}>
+          <Grid2 xs={12} md={6}>
             <Typography>Назва</Typography>
           </Grid2>
 
-          <Grid2 xs={2}>
+          <Grid2 xs={12} md={2}>
             <Typography>Потужність двигуна</Typography>
           </Grid2>
 
-          <Grid2 xs={2}>
+          <Grid2 xs={12} md={2}>
             <Typography>Коробка передач</Typography>
           </Grid2>
 
-          <Grid2 xs={2}>
+          <Grid2 xs={12} md={2}>
             <Typography>Тип трансмісії</Typography>
           </Grid2>
         </Grid2>
 
-        {
-          car.carModification?.map(value => (
-            <Grid2 p={1} key={value.id} container>
-              <Grid2 xs={6}>
-                <Typography>{value.name}</Typography>
-              </Grid2>
+        <Stack divider={<Divider/>}>
+          {
+            car.carModification?.map(value => (
+              <Grid2 p={1} key={value.id} container>
+                <Grid2 xs={12} md={6}>
+                  <Typography>{value.name}</Typography>
+                </Grid2>
 
-              <Grid2 xs={2}>
-                <Typography>{value.powerEngin} л.с</Typography>
-              </Grid2>
+                <Grid2 xs={12} md={2}>
+                  <Typography>{value.powerEngin} л.с</Typography>
+                </Grid2>
 
-              <Grid2 xs={2}>
-                <Typography>{value.wheelDrive}</Typography>
-              </Grid2>
+                <Grid2 xs={12} md={2}>
+                  <Typography>{value.wheelDrive}</Typography>
+                </Grid2>
 
-              <Grid2 xs={2}>
-                <Typography>{value.typeTransmission}</Typography>
+                <Grid2 xs={12} md={2}>
+                  <Typography>{value.typeTransmission}</Typography>
+                </Grid2>
               </Grid2>
-            </Grid2>
-          ))
-        }
+            ))
+          }
+        </Stack>
       </Paper>
 
       <Box mt={2} pb={6}>
